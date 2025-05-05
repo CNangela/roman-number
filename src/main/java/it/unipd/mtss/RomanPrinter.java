cdb0d1ec6a3d28d49de23a53332284514834ba13
angela <angela.canazza@studenti.unipd.it>
2025-05-05 14:13:14 +0200

    aggiunto il codice a RomanPrinter per soddisfare la condizione di test imposta precedentemente, build success close #6

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -2,16 +2,35 @@
 // Angela Canazza 2111030  
 // Linor Sadè 2111942  
 ////////////////////////////////////////////////////////////////////  
+
 package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
-public class RomanPrinter 
-{
-    public static String print(int num )
-    {
-       return null;
+public class RomanPrinter {
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        String[][] asciiI = {
+            {"  _____ "},
+            {" |_   _|"},
+            {"   | |  "},
+            {"   | |  "},
+            {"  _| |_ "},
+            {" |_____|"}
+        };
+
+        StringBuilder asciiArt = new StringBuilder();
+
+        for (int row = 0; row < 6; row++) {
+            for (char c : romanNumber.toCharArray()) {
+                if (c == 'I') {
+                    asciiArt.append(asciiI[row][0]);
+                }
+            }
+            asciiArt.append("\n");
+        }
+
+        return asciiArt.toString();
     }
 }
