e4b55626a127d436419b2aa5c1d7774ad5f404af
angela <angela.canazza@studenti.unipd.it>
2025-05-05 17:01:36 +0200

    modificato il codice per soddisfare i test appena modificati, buid success, close #16

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,12 +6,12 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        if (number < 1 || number > 50) {
-            throw new IntegerToRomanConversionException("Il numero deve essere compreso tra 1 e 50");
+        if (number < 1 || number > 100) {
+            throw new IntegerToRomanConversionException("Il numero deve essere compreso tra 1 e 100");
         }
 
-        String[] romanLetters = { "L", "XL", "X", "IX", "V", "IV", "I" };
-        int[] values = { 50, 40, 10, 9, 5, 4, 1 };
+        String[] romanLetters = { "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        int[] values = { 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
         StringBuilder result = new StringBuilder();
 
